e210b3a4f24c1b615e49b3f6eb39f9a90e6b39f0
yurina-minami <yurinatty373@gmail.com>
2022-05-19 20:39:36 +0900

    Homeのリンク作った

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,44 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div class="links">
+    <div class="link-button">
+      <router-link to="/quiz">Vueクイズ</router-link>
+    </div>
+    <div class="link-button">
+      <router-link to="/palette">Vueパレット</router-link>
+    </div>
+    <div class="link-button">
+      <router-link to="/memo">Vueメモ</router-link>
+    </div>
+  </div>
 </template>
+
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+
+.links {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+
+.link-button {
+  width: 250px;
+  height: 80px;
+  background-color: #42b983;
+
+  font-size: 30px;
+  text-align: center;
+  padding-top: 35px;
+
+  box-shadow: 8px 8px 8px gray;
+}
+
+.link-button:hover {
+  transform: scale(1.05);
+}
+</style>
